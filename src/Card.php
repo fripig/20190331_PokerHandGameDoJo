e89ec8b7e183a61fdb2c293cc15f5a283c7f3288
fripig <fripig@gmail.com>
2019-04-01 01:39:49 +0800

    Card refactor extract number to const

--- src/Card.php
+++ src/Card.php
@@ -4,22 +4,35 @@ namespace App;
 
 class Card
 {
+    const SHAPE             = 4;
+    const HEART             = 3;
+    const DIMAND            = 2;
+    const CLUB              = 1;
+
+    const CARD_ACE          = 1;
+    const CARD_HIGH_ACE     = 14;
+    const CARD_JACK         = 11;
+    const CARD_QUEEN        = 12;
+    const CARD_KING         = 13;
+
+
     private $color;
     private $number;
+
     public function __construct(string $text)
     {
-            $this->color = $this->parseColor($text);
+        $this->color = $this->parseColor($text);
 
-            $this->number = $this->parseWeight($text);
+        $this->number = $this->parseWeight($text);
     }
 
     private function parseColor(string $text)
     {
         $map = [
-            'S' => 4,
-            'H' => 3,
-            'D' => 2,
-            'C' => 1
+            'S' => self::SHAPE,
+            'H' => self::HEART,
+            'D' => self::DIMAND,
+            'C' => self::CLUB
         ];
         $text = strtoupper(trim($text));
         return $map[$text[0]] ?? -1;
@@ -28,14 +41,14 @@ class Card
     private function parseWeight(string $text)
     {
         $map = [
-            'A' => 1,
-            'J' => 11,
-            'Q' => 12,
-            'K' => 13,
+            'A' => self::CARD_ACE,
+            'J' => self::CARD_JACK,
+            'Q' => self::CARD_QUEEN,
+            'K' => self::CARD_KING,
         ];
 
         $text = strtoupper(trim($text));
-        return $map[$text[1]] ?? intval(substr($text,1));
+        return $map[$text[1]] ?? intval(substr($text, 1));
     }
 
     /**
